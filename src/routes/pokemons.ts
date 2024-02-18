805db48ecffd0ee4f3745ec27fbea0bd797b3065
elirium <marko.stojanovic1994@gmail.com>
2024-02-18 23:25:32 +0100

    feat: post and put methods

--- src/routes/pokemons.ts
+++ src/routes/pokemons.ts
@@ -11,21 +11,79 @@ interface Pokemon {
   captured: boolean;
 }
 
-export default async function (fastify: FastifyInstance) {
-  fastify.get('/pokemons', async () => {
-    const { rows }: { rows: Pokemon[] } = await runQuery(
-      fastify.pg,
-      'SELECT * from pokemon;',
-    );
+const getOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            id: { type: 'number' },
+            name: { type: 'string' },
+            type: { type: 'string' },
+            ability: { type: 'string' },
+            height: { type: 'number' },
+            weight: { type: 'number' },
+            base_experience: { type: 'number' },
+            captured: { type: 'boolean' },
+          },
+        },
+      },
+    },
+  },
+};
 
-    if (rows.length === 0) {
-      throw new Error('There is no pokemon to show');
-    }
-    return rows;
-  });
+const postOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      required: [
+        'name',
+        'type',
+        'ability',
+        'height',
+        'weight',
+        'base_experience',
+        'captured',
+      ],
+    },
+    response: {
+      201: {
+        type: 'object',
+        properties: {
+          success: { type: 'boolean' },
+          message: { type: 'string' },
+        },
+      },
+    },
+  },
+};
+
+export default async function (fastify: FastifyInstance) {
+  fastify.get(
+    '/pokemons',
+    getOpts,
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const { rows }: { rows: Pokemon[] } = await runQuery(
+          fastify.pg,
+          'SELECT * from pokemon;',
+        );
+        if (rows.length === 0) {
+          return reply.status(404).send('There is no pokemon to show');
+        }
+        return reply.status(200).send(rows);
+      } catch (error) {
+        console.log(error);
+        return reply.status(500).send({ error: 'Internal Server Error' });
+      }
+    },
+  );
 
   fastify.post(
     '/save-pokemon',
+    postOpts,
     async (req: FastifyRequest, reply: FastifyReply) => {
       const {
         name,
@@ -37,12 +95,56 @@ export default async function (fastify: FastifyInstance) {
         captured,
       }: Pokemon = req.body as Pokemon;
 
-      const result = await runQuery(
-        fastify.pg,
-        'INSERT INTO pokemon (name, type, ability, height, weight, base_experience, captured) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
-        [name, type, ability, height, weight, base_experience, captured],
-      );
-      reply.send({ success: true, id: result.id });
+      try {
+        await runQuery(
+          fastify.pg,
+          'INSERT INTO pokemon (name, type, ability, height, weight, base_experience, captured) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+          [name, type, ability, height, weight, base_experience, captured],
+        );
+        return reply.status(201).send({
+          success: true,
+          message: 'Saved new Pokemon',
+        });
+      } catch (error) {
+        return reply.status(500).send({
+          success: false,
+          message: 'Internal Server Error',
+        });
+      }
+    },
+  );
+
+  fastify.put(
+    '/update-pokemon/:id',
+    postOpts,
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const { id } = req.params as { id: number };
+        const {
+          name,
+          type,
+          ability,
+          height,
+          weight,
+          base_experience,
+          captured,
+        }: Pokemon = req.body as Pokemon;
+
+        await runQuery(
+          fastify.pg,
+          'UPDATE pokemon SET name = $2, type = $3, ability = $4, height = $5, weight = $6, base_experience = $7, captured = $8 WHERE id = $1',
+          [id, name, type, ability, height, weight, base_experience, captured],
+        );
+        return reply.status(201).send({
+          success: true,
+          message: `${name} updated`,
+        });
+      } catch (error) {
+        return reply.status(500).send({
+          success: false,
+          message: 'Internal Server Error',
+        });
+      }
     },
   );
 }
